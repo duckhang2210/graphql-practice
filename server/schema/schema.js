831489ca2cda4e23343f83ce69b210fae497709f
Poet <poet@Poets-MacBook-Pro.local>
2020-02-04 10:39:11 -0600

    fake db

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,13 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+//Dummy db for testing
+var books = [
+  { name: 'Name of the Wind', genre: 'Horror', id: '1' },
+  { name: 'The Final Empire', genre: 'History', id: '2' },
+  { name: 'When the Moon aint shine no more', genre: 'Fantasy', id: '3' }
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -18,6 +26,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         //code to get data from db/other source
+        return _find(books, { id: args.id });
       }
     }
   }
